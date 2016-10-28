f61b12212f8258f955d0a77bb2caaea4de0114d3
Jay Desai <jrdesai@berkeley.edu>
2016-10-28 12:55:46 -0700

    added main class

---
+++ src/main/Main.java
@@ -0,0 +1,22 @@
+package main;
+
+import fileio.*;
+import geometry.*;
+import gifs.*;
+import gradients.*;
+import imgur.*;
+import interactive.*;
+import scene.*;
+import threads.*;
+import utilities.*;
+
+/*
+A class for users to do things with. Fun!
+*/
+public class Main {
+	
+	public static void main(String[] args) {
+	
+	}
+
+}
